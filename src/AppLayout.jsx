a555b7adcaddf962bcc4ded3eeda2cea341cb55d
shroukgamall17 <128715737+shroukgamall17@users.noreply.github.com>
2024-09-22 15:25:54 +0300

    Update AppLayout.jsx

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Todo from "./components/Todos/todo";
+import Todo 
 export default function AppLayout() {
   const lang = useSelector((state) => state.language.language);
   //en ltr
